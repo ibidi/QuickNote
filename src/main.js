f3988eb46ff7b75432e3656f6427fcc0ce0ac784
ibidi <39532511+ibidi@users.noreply.github.com>
2024-08-18 23:18:24 +0300

    Added Firabase Config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
-createApp(App).mount('#app')
+// Firebase yapılandırması
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+}
+
+// Firebase'i başlat
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+// Firebase'i Vue bileşenlerine ekleyelim
+const app = createApp(App)
+app.config.globalProperties.$db = db
+app.mount('#app')
