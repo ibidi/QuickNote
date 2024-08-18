ff5f251fbf0faf0e0838ab1af5380415972c348a
ibidi <39532511+ibidi@users.noreply.github.com>
2024-08-18 22:47:04 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <NoteApp />
+  </div>
+</template>
+
+<script>
+import NoteApp from './components/NoteApp.vue'
+
+export default {
+  components: {
+    NoteApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
